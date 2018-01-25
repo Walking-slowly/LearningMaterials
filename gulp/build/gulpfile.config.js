ebc381328d0d782087cc95dc1cd8adbd049a1297
刘韵 <1601047350@qq.com>
2018-01-25 16:15:33 +0800

    .

---
+++ gulp/build/gulpfile.config.js
@@ -0,0 +1,39 @@
+var SRC_DIR = './src/';     // 源文件目录  
+var DIST_DIR = './dist/';   // 文件处理后存放的目录  
+var DIST_FILES = DIST_DIR + '**'; // 目标路径下的所有文件  
+
+var Config = {
+    src: SRC_DIR,
+    dist: DIST_DIR,
+    dist_files: DIST_FILES,
+    html: {  
+        src: SRC_DIR + '*.html',  
+        dist: DIST_DIR  
+    },
+    json: {  
+        src: SRC_DIR + '*.json',  
+        dist: DIST_DIR  
+    },   
+    assets: {  
+        src: SRC_DIR + 'assets/**/*',            // assets目录：./src/assets  
+        dist: DIST_DIR + 'assets'                // assets文件build后存放的目录：./dist/assets  
+    },  
+    css: {  
+        src: SRC_DIR + 'css/**/*.css',           // CSS目录：./src/css/  
+        dist: DIST_DIR + 'css'                   // CSS文件build后存放的目录：./dist/css  
+    },  
+    sass: {  
+        src: SRC_DIR + 'sass/**/*.scss',         // SASS目录：./src/sass/  
+        dist: DIST_DIR + 'css'                   // SASS文件生成CSS后存放的目录：./dist/css  
+    },  
+    js: {  
+        src: SRC_DIR + 'js/**/*.js',             // JS目录：./src/js/  
+        dist: DIST_DIR + 'js'                   // JS文件build后存放的目录：./dist/js  
+    },  
+    img: {  
+        src: SRC_DIR + 'images/**/*',            // images目录：./src/images/  
+        dist: DIST_DIR + 'images'                // images文件build后存放的目录：./dist/images  
+    }  
+};
+
+module.exports = Config;
