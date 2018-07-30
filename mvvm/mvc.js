f74bc67094cf16a09c6d2fcd57fae52860955536
刘韵 <1601047350@qq.com>
2018-07-30 17:54:18 +0800

    构建watcher

--- mvvm/mvc.js
+++ mvvm/mvc.js
@@ -1,48 +1,62 @@
 // mvvm 入口函数
 var Mvc = /** @class */ (function () {
     function Mvc(mvnData) {
-        new Compile(mvnData.el).init();
-        observer(mvnData.data);
+        this.$mvc = this;
+        this.data = mvnData.data;
+        this.methods = mvnData.methods;
+        new Compile(this.$mvc, mvnData.el).init();
+        new Observer(mvnData.data).init();
     }
     return Mvc;
 }());
-// 数据监听，改变通知订阅者
-function defineProprety(data, key, val) {
-    observer(val);
-    Object.defineProperty(data, // 对象
-    key, // 属性名
-    {
-        enumerable: true,
-        get: function () {
-            return val;
-        },
-        set: function (newVal) {
-            val = newVal; // 当属性被修改时触发
-        }
-    } // 属性描述符 -- 存取描述符
-    );
-}
-// observer 遍历所有属性
-function observer(data) {
-    if (typeof data !== 'object')
-        return;
-    for (var key in data) {
-        defineProprety(data, key, data[key]);
+// 数据劫持，Dep收集改变属性
+var Observer = /** @class */ (function () {
+    function Observer(data) {
+        this.datas = data;
     }
-}
+    Observer.prototype.init = function () {
+        if (typeof this.datas !== 'object')
+            return;
+        for (var key in this.datas) {
+            this.defineProprety(this.datas, key, this.datas[key]);
+        }
+    };
+    Observer.prototype.defineProprety = function (data, key, val) {
+        new Observer(val).init(); // 遍历子属性
+        Object.defineProperty(data, // 对象
+        key, // 属性名
+        {
+            enumerable: true,
+            get: function () {
+                return val;
+            },
+            set: function (newVal) {
+                val = newVal; // 当属性被修改时触发
+            }
+        } // 属性描述符 -- 存取描述符
+        );
+    };
+    return Observer;
+}());
 // compile 模板指令解析 
 var Compile = /** @class */ (function () {
-    function Compile(el) {
+    function Compile($mvc, el) {
+        this.$mvc = $mvc;
         this.ele = document.querySelector(el); // 获取DOM
-        this.fragment = document.createDocumentFragment(); // 创建文档碎片
+        this.fragment = document.createDocumentFragment(); // 创建虚拟文档节点
     }
     Compile.prototype.init = function () {
-        this.clonefragment();
+        this.clonefragment(this.ele);
     };
-    // 将DOM拷贝到文档碎片
-    Compile.prototype.clonefragment = function () {
-        this.fragment.appendChild(this.ele);
+    // 将DOM拷贝到虚拟文档
+    Compile.prototype.clonefragment = function (ele) {
+        var _this = this;
+        var childrens = this.ele.children;
+        Array.prototype.slice.call(childrens).forEach(function (children) {
+            _this.fragment.appendChild(children);
+        });
         this.compileText(this.fragment);
+        this.ele.appendChild(this.fragment);
     };
     // 在文档碎片中处理DOM文本
     Compile.prototype.compileText = function (html) {
@@ -63,22 +77,57 @@ var Compile = /** @class */ (function () {
     };
     // 替换text
     Compile.prototype.replaceText = function (node, reg) {
-        console.log('666', node, reg);
+        console.log('666', node, reg, node.textContent);
         // console.log(text.split('.').indexOf('()'))
     };
     // 指令编译
     Compile.prototype.replaceCompile = function (node) {
+        var _this = this;
         var attrs = node.attributes;
         Array.prototype.slice.call(attrs).forEach(function (attr) {
-            console.log(attr.name, attr.value);
-            // 规则 v-ssss  v-on:clicl
+            var self = _this;
+            // 规则 v-ssss
             if (/[v\-]{2}/.test(attr.name)) {
-                console.log(attr.value);
+                console.log(attr.name, attr.value);
+                if (/[v\-on\:]{5}/.test(attr.name)) {
+                    var method = self.$mvc.methods[attr.value];
+                    node.addEventListener(attr.name.slice(5), method.bind(self.$mvc), false);
+                }
+                else {
+                    var res = self.$mvc.data[attr.value];
+                    switch (attr.name.slice(2)) {
+                        case 'html':
+                            node.innerHTML = res;
+                    }
+                }
             }
-            // 规则 @click
+            // 规则 事件指令@click / v-on:click
             else if (/[\@]/.test(attr.name)) {
+                var method = self.$mvc.methods[attr.value];
+                node.addEventListener(attr.name.slice(1), method.bind(self.$mvc), false);
             }
         });
     };
     return Compile;
+}());
+// 消息订阅者 watcher
+var watcher = /** @class */ (function () {
+    function watcher() {
+    }
+    return watcher;
+}());
+// 管理改变集合
+var Dep = /** @class */ (function () {
+    function Dep() {
+        //改变的数据集合
+        this.subs = [];
+    }
+    //添加改变的数据
+    Dep.prototype.addSub = function (sub) {
+        this.subs.push(sub);
+    };
+    //通知订阅者数据发生变化
+    Dep.prototype.notify = function (newVal) {
+    };
+    return Dep;
 }());
--- mvvm/mvc.ts
+++ mvvm/mvc.ts
@@ -6,36 +6,49 @@ interface MvnData {
 
 // mvvm 入口函数
 class Mvc {
+    $mvc: any
+    data: Object
+    methods?: Object
     constructor (mvnData: MvnData) {
-        new Compile(mvnData.el).init()
-        observer(mvnData.data)
+        this.$mvc = this
+        this.data = mvnData.data
+        this.methods = mvnData.methods
+        new Compile(this.$mvc, mvnData.el).init()
+        new Observer(mvnData.data).init()
     }
 }
 
-// 数据监听，改变通知订阅者
-function defineProprety (data: Object, key: string, val: any): any  {
-    observer(val)
+// 数据劫持，Dep收集改变属性
+class Observer {
+    datas: any
+    constructor (data: any) {
+        this.datas = data
+    }
 
-    Object.defineProperty(
-        data, // 对象
-        key, // 属性名
-        {
-            enumerable: true, // 可枚举属性 允许修改
-            get : function (){
-                return val
-            },
-            set: function (newVal: any) {
-                val = newVal  // 当属性被修改时触发
-            }
-        } // 属性描述符 -- 存取描述符
-    )
-}
+    init () {
+        if (typeof this.datas !== 'object') return;
+        for (var key in this.datas) {
+            this. defineProprety(this.datas, key, this.datas[key])
+        }
+    }
 
-// observer 遍历所有属性
-function observer (data: any): any{
-    if (typeof data !== 'object') return;
-    for (var key in data) {
-        defineProprety(data, key, data[key])
+    defineProprety (data: Object, key: string, val: any): any {
+        new Observer(val).init() // 遍历子属性
+
+        Object.defineProperty(
+            data, // 对象
+            key, // 属性名
+            {
+                enumerable: true, // 可枚举属性 允许修改
+                get : function (): any{
+                    return val
+                },
+                set: function (newVal: any) {
+                    val = newVal  // 当属性被修改时触发
+
+                }
+            } // 属性描述符 -- 存取描述符
+        )
     }
 }
 
@@ -43,21 +56,27 @@ function observer (data: any): any{
 
 // compile 模板指令解析 
 class Compile {
+    $mvc: any
     ele: HTMLElement
     fragment: any
-    constructor (el: string) {
+    constructor ($mvc: any, el: string) {
+        this.$mvc = $mvc
         this.ele = document.querySelector(el) // 获取DOM
-        this.fragment = document.createDocumentFragment() // 创建文档碎片
+        this.fragment = document.createDocumentFragment() // 创建虚拟文档节点
     }
 
     init (): any {
-        this.clonefragment()
+        this.clonefragment(this.ele)
     }
 
-    // 将DOM拷贝到文档碎片
-    clonefragment (): any {
-        this.fragment.appendChild(this.ele)
-        this.compileText(this.fragment)
+    // 将DOM拷贝到虚拟文档
+    clonefragment (ele: Node): any {
+        let childrens = this.ele.children
+        Array.prototype.slice.call(childrens).forEach(children => {
+            this.fragment.appendChild(children)
+        }) // 循环子节点写入虚拟文档
+        this.compileText(this.fragment) // 在虚拟文档中处理操作
+        this.ele.appendChild(this.fragment) // 完成后重新写入真实DOM
     }
 
     // 在文档碎片中处理DOM文本
@@ -70,7 +89,6 @@ class Compile {
             } else if (node.nodeType === 1) {
                 this.replaceCompile(node)
             }
-
             if (node.childNodes && node.childNodes.length > 0) {
                 this.compileText(node)
             }
@@ -80,7 +98,7 @@ class Compile {
     // 替换text
     replaceText (node: Text, reg: string) {
         
-        console.log('666',node, reg)
+        console.log('666',node, reg, node.textContent)
         // console.log(text.split('.').indexOf('()'))
     }
 
@@ -88,19 +106,57 @@ class Compile {
     replaceCompile (node: Element) {
         let attrs = node.attributes
         Array.prototype.slice.call(attrs).forEach( (attr) => {
-            console.log(attr.name, attr.value)
-            // 规则 v-ssss  v-on:clicl
+            let self = this
+            // 规则 v-ssss
             if (/[v\-]{2}/.test(attr.name)) {
-                console.log(attr.value)
-
+                console.log(attr.name,attr.value)
+                if (/[v\-on\:]{5}/.test(attr.name)) {
+                    let method = self.$mvc.methods[attr.value]
+                    node.addEventListener(attr.name.slice(5), method.bind(self.$mvc), false)
+                } else {
+                    let res = self.$mvc.data[attr.value]
+                    switch (attr.name.slice(2)) {
+                        case 'html' :
+                        node.innerHTML = res
+                    }
+                }
             } 
-            // 规则 @click
+            // 规则 事件指令@click / v-on:click
             else if (/[\@]/.test(attr.name)) {
-                
+                let method = self.$mvc.methods[attr.value]
+                node.addEventListener(attr.name.slice(1), method.bind(self.$mvc), false)
             }
-
         })
 
     }
  
+}
+
+
+// 消息订阅者 watcher
+class watcher {
+    constructor () {
+
+    }
+}
+
+
+// 管理改变集合
+class Dep {
+    subs: any[]
+    target: any
+    constructor () {
+        //改变的数据集合
+        this.subs = []
+    }
+
+    //添加改变的数据
+    addSub (sub: any) {
+        this.subs.push(sub)
+    }
+
+    //通知订阅者数据发生变化
+    notify(newVal) {
+
+    }
 }
